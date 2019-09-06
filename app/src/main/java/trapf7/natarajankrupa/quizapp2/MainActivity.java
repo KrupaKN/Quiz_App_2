6a49d239cebfb4e081083785f2000d87de7a69c7
Natarajan <1509914@fcps.edu>
2019-09-06 14:15:30 -0400

    Initial commit

---
+++ app/src/main/java/trapf7/natarajankrupa/quizapp2/MainActivity.java
@@ -0,0 +1,14 @@
+package trapf7.natarajankrupa.quizapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
